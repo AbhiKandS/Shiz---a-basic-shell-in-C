04198ae909feab537d74abcd97202f1e09fc8042
AbhiKandS macbook <abhijithksuks@gmail.com>
2025-03-31 13:37:46 +0530

    Add execute command from stringArray

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 typedef struct strArray {
     char** string;
@@ -9,12 +11,19 @@ typedef struct strArray {
     int size;
 } strArray;
 
-
+strArray strArray_new() {
+    return (strArray) {
+        .string = (char **)malloc(256 * sizeof(char *)),
+        .capacity = 256,
+        .size = 0
+    };
+}
 
 bool strArray_append(strArray *arr, char* str) {
+    arr->size++;
     if (arr->capacity <= arr->size)
     {
-        if (arr->capacity = 0) arr->capacity = arr->size;
+        if (arr->capacity == 0) arr->capacity = arr->size;
         arr->capacity *= 2;
         arr->string = realloc(arr->string, arr->capacity);
         if (!arr->string) {
@@ -23,7 +32,7 @@ bool strArray_append(strArray *arr, char* str) {
         }
     }
 
-    arr->string[arr->size++] = str;
+    arr->string[arr->size - 1] = str;
     return true;
 }
 
@@ -31,6 +40,7 @@ bool strArray_del(strArray *arr) {
     for (int i = 0; i < arr->size; i++) free(arr->string[i]);
     free(arr->string);
     arr->string = NULL;
+    return true;
 }
 
 typedef struct string {
@@ -41,7 +51,7 @@ typedef struct string {
 string scan_string(char *prompt)
 {
     printf("%s", prompt);
-    int i = 0, size = 1;
+    int i = 0, size = 256;
     char *buffer = (char *)malloc(size * sizeof(char *));
     char c;
     while ((c = fgetc(stdin)) != '\n' && c != EOF)
@@ -74,12 +84,35 @@ string scan_string(char *prompt)
 
 
 
-pid_t run_cmd(char** args) {
+pid_t run_cmd(strArray *cmd_args) {
+    pid_t pid = fork();
 
+    strArray_append(cmd_args, NULL);
+
+    if (pid < 0) fprintf(stderr, "ERROR: couldn't fork child process\n");
+
+    if (pid == 0) {
+        execvp(cmd_args->string[0], cmd_args->string);
+        perror("ERROR: Couldn't execute the command\n");
+    } else if (wait(NULL) == -1) {
+        fprintf(stderr, "ERROR: couldn't fork child process\n");
+    } else {
+        printf("back to parent");
+    }
+    return pid;
 }
 
-
 int main() {
+    strArray arr = strArray_new();
+    strArray_append(&arr,"echo");
+    strArray_append(&arr,"hello, world");
+    // for (int i = 0; i < arr.size; i++) {
+    //     printf("%s\n", arr.string[i]);
+    // }
+    run_cmd(&arr);
+}
+
+int main2() {
     do {
         string prompt = scan_string("> ");
         if (
