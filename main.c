cd90489545af63e68e684917cce5a93e3b981331
AbhiKandS macbook <abhijithksuks@gmail.com>
2025-03-31 18:55:57 +0530

    Add evaluate_prompt

--- main.c
+++ main.c
@@ -37,52 +37,104 @@ bool strArray_append(strArray *arr, char* str) {
 }
 
 bool strArray_del(strArray *arr) {
-    for (int i = 0; i < arr->size; i++) free(arr->string[i]);
+    for (int start = 0; start < arr->size; start++) free(arr->string[start]);
     free(arr->string);
     arr->string = NULL;
     return true;
 }
 
 typedef struct string {
+    char* ptr; //allocated pointer
     char* string;
     int size;
+    int capacity;
 } string;
 
-string scan_string(char *prompt)
+string scan_string (char *prompt)
 {
+    //TODO: add invalidation for strings.size > INT_MAX
     printf("%s", prompt);
-    int i = 0, size = 256;
-    char *buffer = (char *)malloc(size * sizeof(char *));
+    int size = 1, capacity = 256;
+    char *buffer = (char *)malloc(capacity * sizeof(char *));
     char c;
-    while ((c = fgetc(stdin)) != '\n' && c != EOF)
+    for (;(c = fgetc(stdin)) != '\n'; size++)
     {
-        if (i + 1 > size)
-        {
-            size *= 2;
-            buffer = realloc(buffer, size);
-        }
+        bool PROGRAM_TERMINATED = (c == EOF);
 
-        buffer[i++] = c;
+        if (PROGRAM_TERMINATED) 
+            exit(0);
+        if (capacity <= size + 1) //+1 for adding '\0'
+        {
+            capacity *= 2;
+            
+            if (!(buffer = realloc(buffer, capacity))) {
+                fprintf(stderr, "ALLOCATION FAILED\n");
+                exit(1);
+            }
+        }
+        buffer[size - 1] = c;
     }
 
-    if (c == EOF) {
-        free(buffer);
-        return (string) {
-            .string = NULL,
-            .size = 0
-        };
-    } else {
-        buffer[i] = '\0';
-        buffer = realloc(buffer, i + 1);
+    capacity = size;
+    size--;
+    buffer[size] = '\0';
+    buffer = realloc(buffer, capacity); //discard unused memory
 
-        return (string){
-            .string = buffer,
-            .size = i
-        };
+    return (string){
+        .ptr = buffer,
+        .string = buffer,
+        .size = size,
+        .capacity = capacity
+    };
+}
+
+void string_trim_left(string *s) {
+    if (s->size == 0 || s->string[0] != ' ') return;
+
+    while (s->string[0] == ' ' && s->size > 0) {
+        s->string++;
+        s->size--;
     }
 }
 
+void string_trim_right(string *s) {
+    if (s->size == 0 || s->string[s->size-1] != ' ') return;
 
+    for (
+        char *c = s->string + s->size - 1;
+        *c == ' ';
+        c--
+    ) *c = '\0';
+}
+
+void string_trim(string *s) {
+    string_trim_left(s);
+    string_trim_right(s);
+}
+
+//just adds a whole bunch of '\0' in place of ' '
+//i mean why not??
+//IMPORTANT!!! there will be overlapping memory between strArray and strings
+strArray evaluate_prompt (string *s) {
+    string_trim(s);
+    strArray arr = strArray_new();
+    int i = 0, j = 0, size;
+    strArray_append(&arr, s->string);
+
+    for (
+        char* c = s->string;
+        *c != '\0';
+        c++
+    ) {
+        //TODO: implementation for support for escape sequence + quotes
+        if (*c == ' ') {
+            *c = '\0';
+            if(*(c+1) != ' ') strArray_append(&arr, c+1);
+        }
+    }
+
+    return arr;
+}
 
 pid_t run_cmd(strArray *cmd_args) {
     pid_t pid = fork();
@@ -103,9 +155,9 @@ pid_t run_cmd(strArray *cmd_args) {
 }
 
 int main() {
-    strArray arr = strArray_new();
-    strArray_append(&arr,"echo");
-    strArray_append(&arr,"hello, world");
+    string s = scan_string(": ");
+    string_trim(&s);
+    strArray arr = evaluate_prompt(&s);
     run_cmd(&arr);
 }
 
